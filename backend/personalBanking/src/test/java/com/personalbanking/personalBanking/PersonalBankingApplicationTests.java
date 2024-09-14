ca8490a0e98c16c96117d866c5cbd539c60fa327
Kushal Borra <kushalborra97@gmail.com>
2024-09-14 13:13:04 +0530

    Springboot setup

---
+++ backend/personalBanking/src/test/java/com/personalbanking/personalBanking/PersonalBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalbanking.personalBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
